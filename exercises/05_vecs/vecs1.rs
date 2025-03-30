647e0b465f8e60a2befb30ba66c48044dae6ca4e
leocui9387 <basic.bear@outlook.com>
2025-03-30 18:15:11 -0400

    Finished a few

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -3,13 +3,22 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
 
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
-    // let v = ???;
+    let v = vec![10,20,30,40];
 
     (a, v)
 }
 
 fn main() {
     // You can optionally experiment here.
+    let (stuff, thing) = array_and_vec();
+    
+    for i in &stuff {
+        println!("STUFF: {i}");
+    }
+
+    for i in &thing {
+        println!("THING: {i}");
+    }
 }
 
 #[cfg(test)]
